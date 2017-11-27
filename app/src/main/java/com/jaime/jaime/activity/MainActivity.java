7d12dae1a6408d27c90fa86590f25c181862da71
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-11-27 10:00:34 -0300

    Tela de Splash implementada e funcionando.

--- app/src/main/java/com/jaime/jaime/activity/MainActivity.java
+++ app/src/main/java/com/jaime/jaime/activity/MainActivity.java
@@ -1,49 +1,42 @@
 package com.jaime.jaime.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.jaime.jaime.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btnProximaPagina;
-    private Intent intent;
-
-    //ATRIBUTOS
-
-
-
-    //METODOS
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pegarReferencias();
-        listenar();
+        /**
+         * IMPLEMENTANDO A TELA DE SPLASH
+         * Tela de introdução temporaria que faz dar um tempo
+         * para que os outros componentes sejam carregados
+         */
+
+        //Escondendo a ActionBar
+        getSupportActionBar().hide();
+
+        // Exibir a Tela em FullScreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Temporizador do Splash
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), CategoriaEstabelecimentosActivity.class));
+                finish(); //Destruindo a Activity
+            }
+        }, 3000);
     }
 
-    private void listenar() {
-        btnProximaPagina.setOnClickListener(this);
-    }
-
-    private void pegarReferencias() {
-        btnProximaPagina = (Button) findViewById(R.id.btn1);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn1:
-                intent = new Intent(MainActivity.this, CategoriaEstabelecimentosActivity.class);
-                startActivity(intent);
-                break;
-        }
-    }
-
-    //Teste Commit Leandro
 }
