4a72959b8801a1f436f9ff4e550394fa179a1787
Leandro Oliveira <leandro.oliveira.ads@outlook.com>
2017-12-02 15:55:26 -0300

    comentando codigo com erros por enquanto

--- app/src/main/java/com/jaime/jaime/adapter/ViewpagerAdapter.java
+++ app/src/main/java/com/jaime/jaime/adapter/ViewpagerAdapter.java
@@ -5,28 +5,30 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.view.View;
 
-import com.jaime.jaime.activity.PageFragment;
+//import com.jaime.jaime.activity.PageFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class ViewpagerAdapter extends FragmentPagerAdapter {
     String[] titles;
-    List<PageFragment> pages = new ArrayList<PageFragment>();
+//    List<PageFragment> pages = new ArrayList<PageFragment>();
 
 
     public ViewpagerAdapter(FragmentManager fm, String[] titles) {
         super(fm);
         this.titles = titles;
-        this.pages.add(new PageFragment());
-        this.pages.add(new PageFragment());
+
+//        this.pages.add(new PageFragment());
+//        this.pages.add(new PageFragment());
     }
 
     @Override
     public Fragment getItem(int position) {
-        return pages.get(position);
+//        return pages.get(position);
+        //Return null por enquanto pq a linha de cima tá dando erro
+        return null;
     }
 
     @Override
@@ -48,5 +50,4 @@ public class ViewpagerAdapter extends FragmentPagerAdapter {
     public int getItemPosition(Object object) {
         return POSITION_NONE;
     }
-}
 }
