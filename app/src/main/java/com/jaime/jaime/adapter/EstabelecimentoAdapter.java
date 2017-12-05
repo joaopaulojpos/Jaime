12de90d9c2682756a939491611cb6246ebd1b45f
Leandro Oliveira <leandro.oliveira.ads@outlook.com>
2017-12-05 00:56:05 -0300

    Correção adapterEstabelecimento

--- app/src/main/java/com/jaime/jaime/adapter/EstabelecimentoAdapter.java
+++ app/src/main/java/com/jaime/jaime/adapter/EstabelecimentoAdapter.java
@@ -1,9 +1,9 @@
 package com.jaime.jaime.adapter;
 
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,8 +30,8 @@ public class EstabelecimentoAdapter extends BaseAdapter {
         this.context = context;
         this.estabelecimentos = estabelecimentos;
     }
-    //METODOS
 
+    //METODOS
 
     @Override
     public int getCount() {
@@ -50,62 +50,35 @@ public class EstabelecimentoAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
-        ViewHolder viewHolder;
-
-
-
-        if (convertView == null) {
-            // Passo 2 Criar View nova
-            view = LayoutInflater.from(context).inflate(R.layout.estabelecimento_item, null);
-            viewHolder = new ViewHolder(view);
-            view.setTag(viewHolder);
-        } else {
-            view = convertView;
-            viewHolder = (ViewHolder) view.getTag();
-        }
-        //Pega o estabelecimento selecionado
+        // Passo 1
         Estabelecimento estabelecimento = estabelecimentos.get(position);
 
+        // Passo 2 Criar View nova
+        convertView = LayoutInflater.from(context).inflate(R.layout.estabelecimento_item, null);
 
-        /*imgEstabelecimento = (ImageView) view.findViewById(R.id.img);
-        ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
-        tvDescricao = (TextView) view.findViewById(R.id.tvDescricao);
-        tvTotalVotos = (TextView) view.findViewById(R.id.tvTotalVotos);
-        tvNome = (TextView) view.findViewById(R.id.tvNome);*/
+        ImageView imgImagem = (ImageView) convertView.findViewById(R.id.img);
+        RatingBar ratingBar = (RatingBar) convertView.findViewById(R.id.ratingBar);
+        TextView tvDescricao = (TextView) convertView.findViewById(R.id.tvDescricao);
+        TextView tvTotalVotos = (TextView) convertView.findViewById(R.id.tvTotalVotos);
+        TextView tvNome = (TextView) convertView.findViewById(R.id.tvNome);
 
-        //Preenche os campos de acordo com o objeto
-        viewHolder.tvNome.setText(estabelecimento.getNome());
-        viewHolder.tvTotalVotos.setText("(" + estabelecimento.getTotalVotos() + ")");
-        viewHolder.tvDescricao.setText(estabelecimento.getDescricao());
-        viewHolder.ratingBar.setMax(5);
-        viewHolder.ratingBar.setRating(estabelecimento.getNotaMedia());
-
-        //Imagem
+        //Passo 3
         Resources res = context.getResources();
-        TypedArray imagens = res.obtainTypedArray(R.array.imagens);
-        Log.i("Leandro", estabelecimento.getImagem() + "");
-        viewHolder.imgEstabelecimento.setImageDrawable(imagens.getDrawable(estabelecimento.getImagem()));
 
+        tvNome.setText(estabelecimento.getNome());
+        ratingBar.setMax(5);
+        ratingBar.setRating(estabelecimento.getNotaMedia());
+        tvDescricao.setText(estabelecimento.getDescricao());
+        tvTotalVotos.setText("(" + estabelecimento.getTotalVotos() + ")");
+
+        //IMAGEM
+        //Pega as imagens que estão no drawable(só as listadas no array.imagens)
+        TypedArray imagens = res.obtainTypedArray(R.array.imagens);
+        //seta no ImageView a imagem no index referente ao estabelecimento, esse index é
+        //o atributo estabelecimento.getImagem()
+        imgImagem.setImageDrawable(imagens.getDrawable(estabelecimento.getImagem()));
 
         // Passo 4
-        return view;
-
-    }
-
-    public class ViewHolder {
-        final ImageView imgEstabelecimento;
-        final RatingBar ratingBar;
-        final TextView tvDescricao;
-        final TextView tvTotalVotos;
-        final TextView tvNome;
-
-        public ViewHolder(View view){
-            tvNome = view.findViewById(R.id.tvNome);
-            imgEstabelecimento = view.findViewById(R.id.imgEstabelecimento);
-            ratingBar = view.findViewById(R.id.ratingBar);
-            tvDescricao = view.findViewById(R.id.tvDescricao);
-            tvTotalVotos = view.findViewById(R.id.tvTotalVotos);
-        }
+        return convertView;
     }
 }
